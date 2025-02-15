316a8ea7961527e0ccaae669333b62985c5479f1
ye4241 <6803102+ye4241@users.noreply.github.com>
2025-02-15 23:04:45 +0800

    add test codes;

--- tests.py
+++ tests.py
@@ -1,9 +1,34 @@
+import os
 import unittest
 from loguru import logger
+from utils import MidjourneyBot, PlaywrightCaptchaSolver, DrissionPageCaptchaSolver
 
 
 class Tests(unittest.IsolatedAsyncioTestCase):
-    async def test_solve_turnstile(self):
-        from utils import BrowserCaptchaSolver
-        solver = BrowserCaptchaSolver(logger)
-        await solver.solve_turnstile('https://2captcha.com/demo/cloudflare-turnstile')
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName)
+        self.__prompt = 'a cat --relax'
+        self.__captcha_solver_args = {
+            'logger': logger,
+            'proxy': os.environ.get('PROXY'),
+            'browser_headless': False,
+            'browser_incognito': True,
+            'yescaptcha_key': os.environ.get('YESCAPTCHA_KEY'),
+            'twocaptcha_key': os.environ.get('TWOCAPTCHA_KEY'),
+        }
+        self.__midjourney_bot_args = {
+            'logger': logger,
+            'token': os.environ.get('DISCORD_TOKEN'),
+            'guild_id': int(os.environ.get('DISCORD_GUILD_ID', 0)),
+            'channel_id': int(os.environ.get('DISCORD_CHANNEL_ID', 0)),
+        }
+
+    async def test_imagine_by_playwright(self):
+        solver = PlaywrightCaptchaSolver(**self.__captcha_solver_args)
+        bot = MidjourneyBot(**self.__midjourney_bot_args, solver=solver)
+        await bot.imagine(self.__prompt)
+
+    async def test_imagine_by_drissonpage(self):
+        solver = DrissionPageCaptchaSolver(**self.__captcha_solver_args)
+        bot = MidjourneyBot(**self.__midjourney_bot_args, solver=solver)
+        await bot.imagine(self.__prompt)
